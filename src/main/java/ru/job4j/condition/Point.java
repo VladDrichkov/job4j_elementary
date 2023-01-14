c86445769e6996cd4856b2707b6a3ce69c69acb9
VladDrichkov <vldrichkov@gmail.com>
2023-01-14 16:35:03 +0700

    7. Расстояние между точками в системе координат [#188]

---
+++ src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Point {
+    public static double distance(int x1, int y1, int x2, int y2) {
+        double result = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double resultDistance = Point.distance(x1, y1, x2, y2);
+        System.out.printf("the distance between two points (%d, %d) and (%d, %d) is %.2f",
+                          x1, y1, x2, y2, resultDistance);
+    }
+}

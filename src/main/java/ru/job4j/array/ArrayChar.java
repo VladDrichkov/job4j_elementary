5a7e4b9050090dff7ad48995780f7d2194351e1c
Влад <vldrichkov@gmail.com>
2025-05-19 11:18:53 +0700

    Слово начинается с ... [#505233 #543748]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,12 +3,12 @@ package ru.job4j.array;
 public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] prefix) {
-        boolean result = true;
+        //boolean result = true;
         for (int i = 0; i < prefix.length; i++) {
             if (prefix[i] != word[i]) {
                 return false;
             }
         }
-        return result;
+        return true;
     }
 }
